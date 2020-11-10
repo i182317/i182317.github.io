5a5b33dab3de51c1c748d3714f549e4491ae4765
Nathan Lefort <nathanlefort53@gmail.com>
2020-11-10 14:56:49 +0100

    mise a jour menu

--- js/compteur.js
+++
@@ -1,9 +0,0 @@
-var intervalID = window.setInterval(myCallback, 500, 'Parameter 1', 'Parameter 2');
-
-function myCallback(a, b)
-{
- // Your code here
- // Parameters are purely optional.
- console.log(a);
- console.log(b);
-}
